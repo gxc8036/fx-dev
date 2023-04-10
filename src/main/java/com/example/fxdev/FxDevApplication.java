c266d3ee8f996ab8e9706b660a948bd80300d3ec
guxichang <19951799881@163.com>
2023-04-10 12:53:16 +0800

    init

---
+++ src/main/java/com/example/fxdev/FxDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.fxdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fxdev/FxDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fxdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
